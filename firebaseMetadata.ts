fff4691f3017cb5d4434a2b5a3a60bf30a8c0c7b
Farman <moabbasi@teksystems.com>
2021-08-30 15:54:55 +0530

    first commit

---
+++ firebaseMetadata.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig= {
+    "apiKey": "AIzaSyA1pGgGbjUZhLNdnCMmFnd5xlig-UjkDYY",
+    "authDomain": "cloud-migration-toolkit.firebaseapp.com",
+    "databaseURL": "https://cloud-migration-toolkit.firebaseio.com",
+    "projectId": "cloud-migration-toolkit",
+    "storageBucket": "cloud-migration-toolkit.appspot.com",
+    "messagingSenderId": "359855519842",
+    "appId": "1:359855519842:web:23582795089d3e8504f93b"
+  };
